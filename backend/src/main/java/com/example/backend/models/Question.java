1032a8f5d1e2272415da7be34bb2a15973e200ce
Wajdi <wajdibenaicha123@gmail.com>
2026-03-04 11:26:59 +0100

    commit1

--- backend/src/main/java/com/example/backend/models/Question.java
+++ backend/src/main/java/com/example/backend/models/Question.java
@@ -1,8 +1,14 @@
 package com.example.backend.models;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+
 import jakarta.persistence.Table;
 
 @Entity
@@ -14,8 +20,17 @@ public class Question {
     public Long id ; 
     public String titre;
     public String type ;
+    @Column(columnDefinition = "VARCHAR(1000)")
+    private String options;
+    
     public Question(){
 
+    }
+    public Question(Long id , String titre , String type){
+        this.id=id;
+        this.titre=titre;
+        this.type=type;
+        this.options = "";
     }
     public Long getId(){
         return id ;
@@ -26,6 +41,9 @@ public class Question {
     public String getType(){
         return type ;
     }
+    public String getOptions(){
+        return options ;
+    }
     public void setId(Long id ){
         this.id=id;
     }
@@ -35,4 +53,14 @@ public class Question {
     public void setType(String type){
         this.type=type;
     }
+    public void addOption(String option) {
+    if (this.options == null || this.options.isEmpty()) this.options = option;
+    else this.options = this.options + "," + option;
+}
+
+public void removeOption(String option) {
+    List<String> list = new ArrayList<>(Arrays.asList(this.options.split(",")));
+    list.remove(option);
+    this.options = String.join(",", list);
+}
 }
