616fbe82d76143de3a088f29dad4f66711eebe70
Wajdi <wajdibenaicha123@gmail.com>
2026-03-09 13:16:42 +0100

    commit1

--- backend/src/main/java/com/example/backend/Repository/GestionnaireRepository.java
+++ backend/src/main/java/com/example/backend/Repository/GestionnaireRepository.java
@@ -1,11 +1,13 @@
 package com.example.backend.Repository;
 
-import com.example.backend.models.Gestionnaire;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import com.example.backend.models.Gestionnaire;
 
 
 @Repository
 public interface GestionnaireRepository extends JpaRepository<Gestionnaire, Long> {
-    
+    Gestionnaire findByEmailAndPassword(String email, String password);
+
 }
