c89d1c65fe309bb33428b42a0470273a06b1b652
Wajdi <wajdibenaicha123@gmail.com>
2026-03-09 11:50:55 +0100

    commit1

--- backend/src/main/java/com/example/backend/controller/AdministrateurController.java
+++ backend/src/main/java/com/example/backend/controller/AdministrateurController.java
@@ -1,13 +1,22 @@
 package com.example.backend.controller;
 
-import com.example.backend.models.Administrateur;
-import com.example.backend.service.AdministrateurService;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import com.example.backend.models.Administrateur;
+import com.example.backend.service.AdministrateurService;
 
 @RestController
 @RequestMapping("/api/administrateurs")
@@ -35,6 +44,14 @@ public class AdministrateurController {
     public ResponseEntity<Administrateur> addAdministrateur(@RequestBody Administrateur administrateur) {
         Administrateur savedAdministrateur = administrateurService.addAdministrateur(administrateur);
         return ResponseEntity.status(HttpStatus.CREATED).body(savedAdministrateur);
+    }
+    @PostMapping("/login")
+    public ResponseEntity<Administrateur> login(@RequestBody Administrateur administrateur) {
+        Administrateur loggedInAdministrateur = administrateurService.login(administrateur.getEmail(), administrateur.getPassword());
+        if (loggedInAdministrateur != null) {
+            return ResponseEntity.ok(loggedInAdministrateur);
+        }
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
     }
 
     @PutMapping("/{id}")
---
+++ frontend/src/app/models/gestionnaire.model.ts
@@ -0,0 +1,6 @@
+export interface Gestionnaire {
+  id: number;
+  email: string;
+  fullName: string;
+  Role : string;
+}
