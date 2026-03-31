2f9f9b04682daf81b4e995c8fdf117e9086d3553
Wajdi <wajdibenaicha123@gmail.com>
2026-03-31 20:24:39 +0100

    update database dump and gitignore

---
+++ database1.sql
@@ -0,0 +1,107 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : mar. 31 mars 2026 à 21:21
+-- Version du serveur : 10.4.32-MariaDB
+-- Version de PHP : 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `database1`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `administrateur`
+--
+-- Erreur de lecture de structure pour la table database1.administrateur : #1932 - Table &#039;database1.administrateur&#039; doesn&#039;t exist in engine
+-- Erreur de lecture des données pour la table database1.administrateur : #1064 - Erreur de syntaxe près de &#039;FROM `database1`.`administrateur`&#039; à la ligne 1
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `clients`
+--
+-- Erreur de lecture de structure pour la table database1.clients : #1932 - Table &#039;database1.clients&#039; doesn&#039;t exist in engine
+-- Erreur de lecture des données pour la table database1.clients : #1064 - Erreur de syntaxe près de &#039;FROM `database1`.`clients`&#039; à la ligne 1
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `gestionnaire`
+--
+-- Erreur de lecture de structure pour la table database1.gestionnaire : #1932 - Table &#039;database1.gestionnaire&#039; doesn&#039;t exist in engine
+-- Erreur de lecture des données pour la table database1.gestionnaire : #1064 - Erreur de syntaxe près de &#039;FROM `database1`.`gestionnaire`&#039; à la ligne 1
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `offre`
+--
+-- Erreur de lecture de structure pour la table database1.offre : #1932 - Table &#039;database1.offre&#039; doesn&#039;t exist in engine
+-- Erreur de lecture des données pour la table database1.offre : #1064 - Erreur de syntaxe près de &#039;FROM `database1`.`offre`&#039; à la ligne 1
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `permission`
+--
+-- Erreur de lecture de structure pour la table database1.permission : #1932 - Table &#039;database1.permission&#039; doesn&#039;t exist in engine
+-- Erreur de lecture des données pour la table database1.permission : #1064 - Erreur de syntaxe près de &#039;FROM `database1`.`permission`&#039; à la ligne 1
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `question`
+--
+-- Erreur de lecture de structure pour la table database1.question : #1932 - Table &#039;database1.question&#039; doesn&#039;t exist in engine
+-- Erreur de lecture des données pour la table database1.question : #1064 - Erreur de syntaxe près de &#039;FROM `database1`.`question`&#039; à la ligne 1
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `questionnaire`
+--
+-- Erreur de lecture de structure pour la table database1.questionnaire : #1932 - Table &#039;database1.questionnaire&#039; doesn&#039;t exist in engine
+-- Erreur de lecture des données pour la table database1.questionnaire : #1064 - Erreur de syntaxe près de &#039;FROM `database1`.`questionnaire`&#039; à la ligne 1
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `questionnaire_questions`
+--
+-- Erreur de lecture de structure pour la table database1.questionnaire_questions : #1932 - Table &#039;database1.questionnaire_questions&#039; doesn&#039;t exist in engine
+-- Erreur de lecture des données pour la table database1.questionnaire_questions : #1064 - Erreur de syntaxe près de &#039;FROM `database1`.`questionnaire_questions`&#039; à la ligne 1
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `reponse`
+--
+-- Erreur de lecture de structure pour la table database1.reponse : #1932 - Table &#039;database1.reponse&#039; doesn&#039;t exist in engine
+-- Erreur de lecture des données pour la table database1.reponse : #1064 - Erreur de syntaxe près de &#039;FROM `database1`.`reponse`&#039; à la ligne 1
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `role`
+--
+-- Erreur de lecture de structure pour la table database1.role : #1932 - Table &#039;database1.role&#039; doesn&#039;t exist in engine
+-- Erreur de lecture des données pour la table database1.role : #1064 - Erreur de syntaxe près de &#039;FROM `database1`.`role`&#039; à la ligne 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
