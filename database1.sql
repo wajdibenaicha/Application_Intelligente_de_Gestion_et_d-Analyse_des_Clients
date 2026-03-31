f208e5e1a004ed201c72a5d94a3e28eca617e765
Wajdi <wajdibenaicha123@gmail.com>
2026-03-31 21:04:21 +0100

    update database schema

--- database1.sql
+++ database1.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : mar. 31 mars 2026 à 21:21
+-- Généré le : mar. 31 mars 2026 à 22:03
 -- Version du serveur : 10.4.32-MariaDB
 -- Version de PHP : 8.2.12
 
@@ -26,80 +26,299 @@ SET time_zone = "+00:00";
 --
 -- Structure de la table `administrateur`
 --
--- Erreur de lecture de structure pour la table database1.administrateur : #1932 - Table &#039;database1.administrateur&#039; doesn&#039;t exist in engine
--- Erreur de lecture des données pour la table database1.administrateur : #1064 - Erreur de syntaxe près de &#039;FROM `database1`.`administrateur`&#039; à la ligne 1
+
+CREATE TABLE `administrateur` (
+  `id` bigint(20) NOT NULL,
+  `full_name` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `administrateur`
+--
+
+INSERT INTO `administrateur` (`id`, `full_name`, `password`, `email`) VALUES
+(1, 'Admin', 'admin', 'admin@admin.com');
 
 -- --------------------------------------------------------
 
 --
 -- Structure de la table `clients`
 --
--- Erreur de lecture de structure pour la table database1.clients : #1932 - Table &#039;database1.clients&#039; doesn&#039;t exist in engine
--- Erreur de lecture des données pour la table database1.clients : #1064 - Erreur de syntaxe près de &#039;FROM `database1`.`clients`&#039; à la ligne 1
+
+CREATE TABLE `clients` (
+  `id` bigint(20) NOT NULL,
+  `mail` varchar(255) DEFAULT NULL,
+  `tel` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
 -- Structure de la table `gestionnaire`
 --
--- Erreur de lecture de structure pour la table database1.gestionnaire : #1932 - Table &#039;database1.gestionnaire&#039; doesn&#039;t exist in engine
--- Erreur de lecture des données pour la table database1.gestionnaire : #1064 - Erreur de syntaxe près de &#039;FROM `database1`.`gestionnaire`&#039; à la ligne 1
+
+CREATE TABLE `gestionnaire` (
+  `id` bigint(20) NOT NULL,
+  `full_name` varchar(255) NOT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `id_role` bigint(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
 -- Structure de la table `offre`
 --
--- Erreur de lecture de structure pour la table database1.offre : #1932 - Table &#039;database1.offre&#039; doesn&#039;t exist in engine
--- Erreur de lecture des données pour la table database1.offre : #1064 - Erreur de syntaxe près de &#039;FROM `database1`.`offre`&#039; à la ligne 1
+
+CREATE TABLE `offre` (
+  `id` bigint(20) NOT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
 -- Structure de la table `permission`
 --
--- Erreur de lecture de structure pour la table database1.permission : #1932 - Table &#039;database1.permission&#039; doesn&#039;t exist in engine
--- Erreur de lecture des données pour la table database1.permission : #1064 - Erreur de syntaxe près de &#039;FROM `database1`.`permission`&#039; à la ligne 1
+
+CREATE TABLE `permission` (
+  `id` bigint(20) NOT NULL,
+  `description` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
 -- Structure de la table `question`
 --
--- Erreur de lecture de structure pour la table database1.question : #1932 - Table &#039;database1.question&#039; doesn&#039;t exist in engine
--- Erreur de lecture des données pour la table database1.question : #1064 - Erreur de syntaxe près de &#039;FROM `database1`.`question`&#039; à la ligne 1
+
+CREATE TABLE `question` (
+  `id` bigint(20) NOT NULL,
+  `titre` varchar(255) NOT NULL,
+  `type` varchar(255) NOT NULL,
+  `options` varchar(5000) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
 -- Structure de la table `questionnaire`
 --
--- Erreur de lecture de structure pour la table database1.questionnaire : #1932 - Table &#039;database1.questionnaire&#039; doesn&#039;t exist in engine
--- Erreur de lecture des données pour la table database1.questionnaire : #1064 - Erreur de syntaxe près de &#039;FROM `database1`.`questionnaire`&#039; à la ligne 1
+
+CREATE TABLE `questionnaire` (
+  `id` bigint(20) NOT NULL,
+  `titre` varchar(255) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `gestionnaire_id` bigint(20) DEFAULT NULL,
+  `confirmed` tinyint(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
 -- Structure de la table `questionnaire_questions`
 --
--- Erreur de lecture de structure pour la table database1.questionnaire_questions : #1932 - Table &#039;database1.questionnaire_questions&#039; doesn&#039;t exist in engine
--- Erreur de lecture des données pour la table database1.questionnaire_questions : #1064 - Erreur de syntaxe près de &#039;FROM `database1`.`questionnaire_questions`&#039; à la ligne 1
+
+CREATE TABLE `questionnaire_questions` (
+  `questionnaire_id` bigint(20) NOT NULL,
+  `question_id` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
 -- Structure de la table `reponse`
 --
--- Erreur de lecture de structure pour la table database1.reponse : #1932 - Table &#039;database1.reponse&#039; doesn&#039;t exist in engine
--- Erreur de lecture des données pour la table database1.reponse : #1064 - Erreur de syntaxe près de &#039;FROM `database1`.`reponse`&#039; à la ligne 1
+
+CREATE TABLE `reponse` (
+  `id` bigint(20) NOT NULL,
+  `reponse` varchar(255) DEFAULT NULL,
+  `client_id` bigint(20) DEFAULT NULL,
+  `question_id` bigint(20) DEFAULT NULL,
+  `questionnaire_id` bigint(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
 -- Structure de la table `role`
 --
--- Erreur de lecture de structure pour la table database1.role : #1932 - Table &#039;database1.role&#039; doesn&#039;t exist in engine
--- Erreur de lecture des données pour la table database1.role : #1064 - Erreur de syntaxe près de &#039;FROM `database1`.`role`&#039; à la ligne 1
+
+CREATE TABLE `role` (
+  `id` bigint(20) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `id_permission` bigint(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `administrateur`
+--
+ALTER TABLE `administrateur`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `clients`
+--
+ALTER TABLE `clients`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `gestionnaire`
+--
+ALTER TABLE `gestionnaire`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_role` (`id_role`);
+
+--
+-- Index pour la table `offre`
+--
+ALTER TABLE `offre`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `permission`
+--
+ALTER TABLE `permission`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `question`
+--
+ALTER TABLE `question`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `questionnaire`
+--
+ALTER TABLE `questionnaire`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `gestionnaire_id` (`gestionnaire_id`);
+
+--
+-- Index pour la table `questionnaire_questions`
+--
+ALTER TABLE `questionnaire_questions`
+  ADD PRIMARY KEY (`questionnaire_id`,`question_id`),
+  ADD KEY `question_id` (`question_id`);
+
+--
+-- Index pour la table `reponse`
+--
+ALTER TABLE `reponse`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `client_id` (`client_id`),
+  ADD KEY `question_id` (`question_id`),
+  ADD KEY `questionnaire_id` (`questionnaire_id`);
+
+--
+-- Index pour la table `role`
+--
+ALTER TABLE `role`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_permission` (`id_permission`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `administrateur`
+--
+ALTER TABLE `administrateur`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT pour la table `clients`
+--
+ALTER TABLE `clients`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `gestionnaire`
+--
+ALTER TABLE `gestionnaire`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `offre`
+--
+ALTER TABLE `offre`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `permission`
+--
+ALTER TABLE `permission`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `question`
+--
+ALTER TABLE `question`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `questionnaire`
+--
+ALTER TABLE `questionnaire`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `reponse`
+--
+ALTER TABLE `reponse`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `role`
+--
+ALTER TABLE `role`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `gestionnaire`
+--
+ALTER TABLE `gestionnaire`
+  ADD CONSTRAINT `gestionnaire_ibfk_1` FOREIGN KEY (`id_role`) REFERENCES `role` (`id`);
+
+--
+-- Contraintes pour la table `questionnaire`
+--
+ALTER TABLE `questionnaire`
+  ADD CONSTRAINT `questionnaire_ibfk_1` FOREIGN KEY (`gestionnaire_id`) REFERENCES `gestionnaire` (`id`);
+
+--
+-- Contraintes pour la table `questionnaire_questions`
+--
+ALTER TABLE `questionnaire_questions`
+  ADD CONSTRAINT `questionnaire_questions_ibfk_1` FOREIGN KEY (`questionnaire_id`) REFERENCES `questionnaire` (`id`),
+  ADD CONSTRAINT `questionnaire_questions_ibfk_2` FOREIGN KEY (`question_id`) REFERENCES `question` (`id`);
+
+--
+-- Contraintes pour la table `reponse`
+--
+ALTER TABLE `reponse`
+  ADD CONSTRAINT `reponse_ibfk_1` FOREIGN KEY (`client_id`) REFERENCES `clients` (`id`),
+  ADD CONSTRAINT `reponse_ibfk_2` FOREIGN KEY (`question_id`) REFERENCES `question` (`id`),
+  ADD CONSTRAINT `reponse_ibfk_3` FOREIGN KEY (`questionnaire_id`) REFERENCES `questionnaire` (`id`);
+
+--
+-- Contraintes pour la table `role`
+--
+ALTER TABLE `role`
+  ADD CONSTRAINT `role_ibfk_1` FOREIGN KEY (`id_permission`) REFERENCES `permission` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
